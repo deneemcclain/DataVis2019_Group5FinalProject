466ac57fa2f165a3e068f4457f2b75f8cef42d05
Denee McClain <deneemcclain@yahoo.com>
2019-12-13 12:40:39 -0600

    Adding files

---
+++ Group5_FinalProject/Group5_PreProcessing.R
@@ -0,0 +1,142 @@
+# Data Visualization
+# Fall 2019
+# Group 5 Final Project - Preprocessing Code
+
+setwd("C:/Users/denee/Documents/University of Notre Dame Data Science/Fall 2019/Data Visualization/Assignments/Group5_FinalProject")
+# Load libraries
+library(dplyr)
+library(rgdal)
+library(stringr)
+library(ggmap)
+# Geocode register
+register_google(key = "AIzaSyAe2Brb2eyF0ZeyEncpk_36JQkC-o9Xyvg")
+
+## Street Lights & Abandoned Properties Tab ##
+# load abandoned buildings data
+abandoned <- readOGR(dsn="Abandoned_Property_Parcels", 
+                     layer = "Abandoned_Property_Parcels", stringsAsFactors = FALSE)
+buildings <- abandoned@data
+
+######### CODE TO REVERSE GEOCODE STREET LIGHTS DATA ###########
+# lights <- read.csv("Street_Lights.txt")
+# lights$latlon <- paste(lights$Lon, lights$Lat, sep = " ")
+# zips <- vector(mode = "character", length = nrow(lights))
+# for (i in 1:nrow(lights)) {
+#   light <- lights[i, ]
+#   latlon_s <- as.numeric(str_split_fixed(light['latlon'], " ", n = 2))
+#   zips[i] <- str_extract(revgeocode(latlon_s), "(?!^)([0-9]){5}")
+# }
+# lights$zipcodes <- zips
+######### CODE TO REVERSE GEOCODE STREET LIGHTS DATA ###########
+
+load("lights_revgeocoded.RData") #- necessary on reruns because above section runs long
+
+# select necessary columns
+lights <- lights %>%
+  select(OBJECTID, Pole_Num_1, Bulb_Type, Wattage, Pole_Type, zipcodes) %>% 
+  na.omit()
+# clean data
+lights$Bulb_Type <- str_trim(lights$Bulb_Type)
+lights$Bulb_Type <- 
+  str_replace_all(lights$Bulb_Type,
+                  "Yellow-H.P. Sodium|Yellow-H.P Sodium|Yellow- H.P. Sodium|Yellow- H.P. Spdium|Yellow H.P Sodium",
+                  "Yellow - H.P. Sodium")
+lights$Bulb_Type <- 
+  str_replace_all(lights$Bulb_Type,
+                  "H.p. Sodium",
+                  "H.P. Sodium")
+lights$Bulb_Type <- 
+  str_replace_all(lights$Bulb_Type,
+                  "HPs",
+                  "HPS")
+lights$Wattage <- str_replace_all(lights$Wattage,
+                                          "[^0-9\\-]",
+                                          "")
+
+save(lights, file = "lights_clean.RData")
+save(buildings, file = "buildings_clean.RData")
+## Street Lights & Abandoned Properties Tab ##
+
+## Business Info Tab ##
+# Read Geocoded Business Data from file
+Businesses <- read_csv("Geocoded_Business_Licenses.txt")
+
+BusinessesClean <- na.omit(Businesses) %>% filter(State == "IN")
+
+# Read Class Ref Sheet
+classref <- read_csv("ClassRef.csv")
+
+# Join data onto df
+BusinessesClean <- left_join(BusinessesClean,classref)
+
+# Create Spatial Business Data
+businesses.spatial <- SpatialPointsDataFrame(coords = BusinessesClean[,c("lon","lat")], 
+                                             data = BusinessesClean,
+                                             proj4string = CRS("+proj=longlat +datum=WGS84"))
+
+# Create Popup Object
+businesses.spatial$popup <- paste("<b>",businesses.spatial$Business_Name,"</b><br>",
+                                  "Category:",businesses.spatial$Category,"<br>",
+                                  "Type: ",businesses.spatial$Classification_Description,"<br>",
+                                  "Phone: ",businesses.spatial$Business_Phone_Number,sep ="")
+
+# Write to file
+save(businesses.spatial,file = "CleanedBusinessed.Rdata")
+
+# Get and Clean Census Data
+
+# Set Up API key (one time run)
+# tidycensus::census_api_key("cacaf1ff1d0fdf9bc06a72427eded4c164cd0d08", install = T)
+
+# Select Variables
+
+vars = c("total.population" = "B01001_001", 
+         "housng.units" = "B25001_001",
+         "median.age" = "B01002_001",
+         "median.income" = "B07011_001")
+
+# Get data by zip code for all US from ACS 2017 
+zip_census <- get_acs(geography = "zcta", 
+                      variables = vars, 
+                      year = 2017, 
+                      geometry = T, 
+                      output = "wide")
+
+# Filter to just the zip codes for South Bend
+sb_index <- (zip_census$GEOID == 46628 | zip_census$GEOID == 46613 | zip_census$GEOID == 46601 | 
+               zip_census$GEOID == 46635 | zip_census$GEOID == 46617 |zip_census$GEOID == 46619 | 
+               zip_census$GEOID == 46615 | zip_census$GEOID == 46614 | zip_census$GEOID == 46637 |
+               zip_census$GEOID == 46616 | zip_census$GEOID == 46680 | zip_census$GEOID == 46624 |
+               zip_census$GEOID == 46634 | zip_census$GEOID == 46660)
+
+# Transform for plotting w/ leaflet
+sb_zip_data <- zip_census[sb_index,] %>% st_transform(crs = "+init=epsg:4326")
+
+# Create Popup Object
+sb_zip_data$popup <- paste("<b>",sb_zip_data$GEOID,"</b><br>",
+                           "Population: ",sb_zip_data$total.populationE,"<br>")
+
+# Save File
+save(sb_zip_data, file = "SB_Zip_Data.Rdata")
+## Business Info Tab ##
+
+## Code Violations Tab ##
+dat <- read.csv("Code_Enforcement_Cases.txt")
+
+dat$Case_Year[dat$Case_Year == 8] <- 2008
+dat$Case_Year[dat$Case_Year == 9] <- 2009
+dat$Case_Year[dat$Case_Year == 10] <- 2010
+dat$Case_Year[dat$Case_Year == 11] <- 2011
+dat$Case_Year[dat$Case_Year == 12] <- 2012
+dat$Case_Year[dat$Case_Year == 13] <- 2013
+dat$Case_Year[dat$Case_Year == 14] <- 2014
+dat$Zip_Code[dat$Zip_Code == 0] <- "Unknown"
+
+codeViolations <- dat %>% select(-Case_Status_Code, -Date_Case_Reported___Calc, -State_Code, -City)
+markers <- SpatialPointsDataFrame(coords = codeViolations[,c("Lon", "Lat")], data = codeViolations,
+                                  proj4string = CRS("+proj=longlat +datum=WGS84"))
+
+save(codeViolations, file = "codeViolations_clean.RData")
+save(markers, file = "markers_clean.RData")
+## Code Violations Tab ##
+
---
+++ Group5_FinalProject/app.R
@@ -0,0 +1,279 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(DT)
+library(shiny)
+library(dplyr)
+library(leaflet)
+library(sp)
+library(ggmap)
+library(tidyverse)
+library(sf)
+library(tidycensus)
+
+#set working directory
+#setwd("C:/Users/denee/Documents/University of Notre Dame Data Science/Fall 2019/Data Visualization/Assignments/Group5_FinalProject")
+# load preprocessed data
+load("lights_clean.RData")
+load("buildings_clean.RData")
+load("codeViolations_clean.RData")
+load("markers_clean.RData")
+load("CleanedBusinessed.Rdata")
+load("SB_Zip_Data.Rdata")
+
+# Create Pallette Objects for Plotting
+
+# Options for Population
+pal <- colorQuantile(palette = "viridis", domain = sb_zip_data$total.populationE)
+pal2 <-colorNumeric(palette = "Reds", domain = sb_zip_data$total.populationE)
+pal3 <- colorNumeric(palette = "plasma", domain = sb_zip_data$total.populationE)
+
+# Options for Business Type Coloring
+pal4 <- colorFactor(palette = 'Set1', domain = businesses.spatial$Category)
+
+# Define UI for application that draws a histogram
+ui <- navbarPage("Potential Problem Areas in South Bend, IN",
+   tabPanel("About", 
+            "This project was completed by Courtney Cosgrove, Denee McClain, and Jay Richardson. In it, we explore various opportunities for a greater commitment of resources. We present this data to the mayor of South Bend and his staff in hopes that they will identify problem areas and areas for improvement in the city of South Bend."),
+   tabPanel("Distribution of Abandoned Buildings and Street Lights by Zip Code",
+            sidebarLayout(
+              sidebarPanel(
+                selectInput("zipcode", "Zip Code", c("All", unique(lights$zipcodes))),
+                selectInput("bulbtype", "Bulb Type", c("All", unique(lights$Bulb_Type))),
+                selectInput("wattage", "Wattage", c("All", unique(lights$Wattage))),
+                selectInput("poletype", "Pole Type", c("All", unique(as.character(lights$Pole_Type))))
+              ),
+              
+              # Show a plot of the generated distribution
+              mainPanel(
+                plotOutput("abandonedLights")
+              )
+            )),
+   tabPanel("Demographic And Business Info", 
+            sidebarLayout(
+              
+              sidebarPanel(
+                selectInput("zipselection",
+                            "Select Zip Code:",
+                            choices = c("All",unique(sb_zip_data$GEOID))
+                ),
+                selectInput("TypeSelection",
+                            "Select Business Type:",
+                            choices = c("All",unique(businesses.spatial$Category)))
+              ), #End sidebar panel
+              
+              # Show a plot of the generated distribution
+              mainPanel(
+                leafletOutput("map")
+              ) # End Main Panel
+            )),
+   tabPanel("City of South Bend - Code Enforcement Violations", 
+            fluidPage(
+              
+              leafletOutput("mymap"),
+              
+              fluidRow(
+                column(4,
+                       selectInput("Case_Status_Code_Description",
+                                   "Code Status:",
+                                   c("All",
+                                     unique(as.character(codeViolations$Case_Status_Code_Description))))
+                ),
+                column(4,
+                       selectInput("Case_Month",
+                                   "Month:",
+                                   c("All",
+                                     unique(as.character(codeViolations$Case_Month))))
+                ),
+                column(4,
+                       selectInput("Zip_Code",
+                                   "Zip Code:",
+                                   c("All",
+                                     unique(as.character(codeViolations$Zip_Code))))
+                ),
+                column(4,
+                       selectInput("Case_Type_Code_Description",
+                                   "Violation Type:",
+                                   c("All",
+                                     unique(as.character(codeViolations$Case_Type_Code_Description))))
+                ),
+                column(4,
+                       selectInput("Case_Year",
+                                   "Year:",
+                                   c("All",
+                                     unique(as.character(codeViolations$Case_Year))))
+                )
+              ),
+              DT::dataTableOutput("table")
+            ))
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  ## Abandoned Properties Tab
+  output$abandonedLights <- renderPlot({
+    if (input$zipcode == "All") {
+      theselights <- lights
+      thesebuildings <- buildings
+    }
+    else {
+      theselights <- lights %>% filter(zipcodes == input$zipcode)
+      thesebuildings <- buildings %>% filter(Zip_Code == input$zipcode)
+    }
+    
+    if (input$bulbtype == "All") {
+      theselights <- theselights
+    }
+    else {
+      theselights <- theselights %>% filter(Bulb_Type == input$bulbtype)
+    }
+    
+    if (input$wattage == "All") {
+      theselights <- theselights
+    }
+    else {
+      theselights <- theselights %>% filter(Wattage == input$wattage)
+    }
+    
+    if (input$poletype == "All") {
+      theselights <- theselights
+    }
+    else {
+      theselights <- theselights %>% filter(Pole_Type == input$poletype)
+    }
+    # put count information into data frame
+    lightsandbuildings <- data.frame(name = c("Abandoned Buildings", "Street Lights"),
+                                     Count = c(nrow(thesebuildings), nrow(theselights)))
+    
+    # draw the histogram with the specified number of bins
+    ggplot(data = lightsandbuildings, 
+           aes(x = name, y = Count, fill = name)) + 
+      geom_col(show.legend = F) + xlab("") + 
+      scale_fill_manual(values = c("#9e5002", "#fcd303"))
+  })
+  ## Abandoned Properties Tab
+  
+  ## Code Enforcements Tab
+  output$mymap <- renderLeaflet({
+    leaflet(options = leafletOptions(preferCanvas = TRUE)) %>%
+      addProviderTiles(providers$Esri.NatGeoWorldMap, options = providerTileOptions(
+        updateWhenZooming = FALSE,      # map won't update tiles until zoom is done
+        updateWhenIdle = TRUE           # map won't load new tiles when panning
+      )) %>%
+      addMarkers(data = filtered(), clusterOptions = markerClusterOptions())
+  })
+  
+  output$table <- DT::renderDataTable({
+    filtered()
+  })
+  
+  filtered <- reactive({
+    temp <- codeViolations
+    if (input$Case_Status_Code_Description != "All") {
+      temp <- temp[temp$Case_Status_Code_Description == input$Case_Status_Code_Description,]
+    }
+    if (input$Case_Year != "All") {
+      temp <- temp[temp$Case_Year == input$Case_Year,]
+    }
+    if (input$Case_Month != "All") {
+      temp <- temp[temp$Case_Month == input$Case_Month,]
+    }
+    if (input$Zip_Code != "All") {
+      temp <- temp[temp$Zip_Code == input$Zip_Code,]
+    }
+    if (input$Case_Type_Code_Description != "All") {
+      temp <- temp[temp$Case_Type_Code_Description == input$Case_Type_Code_Description,]
+    }
+    return(temp)
+  })
+  
+  observe({
+    leafletProxy(mapId = "mymap", data = filtered()) %>%
+      clearMarkers() %>%
+      addMarkers(data = filtered(), clusterOptions = markerClusterOptions())
+  })
+  ## Code Enforcements Tab
+  
+  ## Business Info Tab
+  output$map <- renderLeaflet({
+    
+    filtered_bus <- reactive({
+      filtered_obj <- businesses.spatial
+      if(input$zipselection != "All") {
+        filtered_obj <- businesses.spatial[businesses.spatial$Zip_Code == input$zipselection,]
+        if(input$TypeSelection != "All"){
+          filtered_obj <- filtered_obj[filtered_obj$Category == input$TypeSelection,]
+        }
+        if(input$TypeSelection == "All"){
+          filtered_obj <- businesses.spatial[businesses.spatial$Zip_Code == input$zipselection,]
+        }
+      }
+      if(input$zipselection == "All"){
+        filtered_obj <- businesses.spatial
+        if(input$TypeSelection != "All"){
+          filtered_obj <- filtered_obj[filtered_obj$Category == input$TypeSelection,]
+        }
+        if(input$TypeSelection == "All"){
+          filtered_obj <- businesses.spatial
+        }
+      }
+      return(filtered_obj)
+    })
+    
+    filtered_map <- reactive({
+      filtered_obj <- sb_zip_data
+      if(input$zipselection != "All") {
+        filtered_obj <- sb_zip_data[sb_zip_data$GEOID == input$zipselection,]
+      }
+      return(filtered_obj)
+    })
+    
+    leaflet()  %>%
+      addTiles(group = "Basic")  %>%
+      addPolygons(data = filtered_map(),
+                  group = "Population Fill",
+                  stroke = T,
+                  popup = ~popup,
+                  color = ~ pal2(total.populationE),
+                  fillOpacity = 0.7,
+                  opacity = 1, 
+                  fill = T, 
+                  weight = 1) %>% 
+      addPolylines(data = filtered_map(),
+                   group = "ZipLines",
+                   color = "black",
+                   opacity = 1,
+                   weight = 2) %>% 
+      addCircleMarkers(data = filtered_bus(), 
+                       popup = ~popup,
+                       color = ~pal4(Category),
+                       stroke = 0,
+                       fillOpacity = 0.4, 
+                       radius = 3,
+                       group = "Businesses") %>% 
+      addLegend("bottomright", 
+                pal = pal2, 
+                values = sb_zip_data$total.populationE,
+                title = "Population",
+                group = "Pop Legend") %>% 
+      addLegend("bottomleft",
+                pal = pal4,
+                values = businesses.spatial$Category,
+                title = "Business Types",
+                group = "Business Legend") %>% 
+      addLayersControl(
+        overlayGroups= c("Population Fill","Businesses"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+  ## Business Info Tab
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
